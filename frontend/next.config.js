9e96b2ec1ac2d89449009e8e94b242549c536c9c
malindasilva047-gif <malindasilva047@gmail.com>
2026-04-07 22:34:55 +0530

    updated

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -44,8 +44,12 @@ const nextConfig = {
         source: "/(.*)",
         headers: [
           {
-            key: "Content-Security-Policy",
-            value: "upgrade-insecure-requests"
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin-allow-popups"
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "unsafe-none"
           }
         ]
       }
